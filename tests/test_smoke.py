780d5baeb00b9bb9090fbf0854ffb7ea43f40596
daniil <dt.bedrin@interfax.ru>
2025-11-01 15:44:44 +0300

    getting the response body after the status

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -8,7 +8,7 @@ import requests
 def test_smoke_status(app_url):
     response = requests.get(f"{app_url}/status")
     print('service_available')
-    data = response.json()
     assert response.status_code == 200, f"Ожидался статус 200, получен {response.status_code}"
+    data = response.json()
     assert "users" in data, "Ответ не содержит ключ 'users'"
     assert data["users"] is True, "Сервис 'users' недоступен"
